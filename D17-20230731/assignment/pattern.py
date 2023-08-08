f92981e19130575c71b3c55c1989d440eb45e17a
BabyShali28 <shalibaby15@gmail.com>
2023-08-08 17:10:56 +0530

    karka-training

---
+++ D17-20230731/assignment/pattern.py
@@ -0,0 +1,66 @@
+# for i in range(n):
+    
+#     print("x"*n)
+
+
+
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         print("x",end='')
+#     print('')
+
+     
+
+#
+
+# n=5
+# count=0
+# for i in range(1,):
+    
+#     for j in range(n):
+        
+#         print(count,end='')
+#     print('')
+
+#####1 to 25
+# n=5
+# for i in range(1,(n*n)+1):
+#     print(i," ",end='')
+#     if i%n==0:
+#         print('\n')
+    
+
+######reverse
+# n=5
+# for i in range((n*n),0,-1):
+    
+    
+#     if i%n==0:
+#         print('\n')
+#     print(i,end=' ')
+
+
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print("x",end='')
+#     print(' ')
+
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("x",end='')
+#     print(' ')
+
+
+n=5
+for i in range(n,0,-1):
+    for j in range(i):
+        print("x",end='')
+    print(' ')
+
+
+
+
