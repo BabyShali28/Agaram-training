35c60f109cf65abc3fb2c6f42db4f3fa9f2b9ce8
BabyShali28 <shalibaby15@gmail.com>
2023-08-09 16:41:38 +0530

    karka-training

--- D17-20230731/assignment/pattern.py
+++ D17-20230731/assignment/pattern.py
@@ -55,11 +55,49 @@
 #     print(' ')
 
 
-n=5
-for i in range(n,0,-1):
-    for j in range(i):
-        print("x",end='')
-    print(' ')
+# n=5
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("x",end='')
+#     print(' ')
+
+#Answer : 1     
+
+# given  = [1,0,2,3,0,4,0,5,0]
+# res = []
+# for i in given: 
+#     if len(res) < len(given) : 
+
+#         if i == 0 and len(res) < (len(given)-1) :
+#                 res+=[i,i]
+#         else: 
+#             res+=[i]
+    
+#     else : 
+#           break
+
+# print('result', res)
+
+
+
+
+
+stri= "the quick brown fox jumps over the lazy dog the quick brown fox"
+total={}
+def frequent(strin):
+    
+    st=strin.split()
+    for a in st:
+        
+        if a not in strin:
+            
+            total.update({a:1})
+        else:
+            total[a]+=1
+          
+    print(total)
+
+frequent(stri)
 
 
 
